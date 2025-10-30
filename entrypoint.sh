90ab22a55fed9372fad1e8a77b7bdf9ea4849581
x80486 <34633617+x80486@users.noreply.github.com>
2025-10-30 11:25:11 -0400

    Add structured startup debug logging, configurable via environment variables

---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+
+export ELECTRON_OZONE_PLATFORM_HINT="auto"
+
+ADDITIONAL_ARGS=()
+
+if [[ -n "${FLATPAK_ID:-}" && -n "${XDG_RUNTIME_DIR:-}" ]]; then
+  export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
+fi
+
+if [[ "${XDG_SESSION_TYPE:-}" == "wayland" ]]; then
+  ADDITIONAL_ARGS+=("--enable-wayland-ime" "--wayland-text-input-version=3")
+fi
+
+if [[ "${SEJDA_DEBUG_ENABLED:-}" == "true" ]]; then
+  printf "ADDITIONAL_ARGS: [%s]\n" "${ADDITIONAL_ARGS[*]:-}"
+  printf "TMPDIR: %s\n" "${TMPDIR:-}"
+  printf "XDG_CURRENT_DESKTOP: %s\n" "${XDG_CURRENT_DESKTOP:-}"
+  printf "XDG_SESSION_TYPE: %s\n" "${XDG_SESSION_TYPE:-}"
+fi
+
+exec zypak-wrapper "/app/extra/sejda-desktop/sejda-desktop" "${ADDITIONAL_ARGS[@]}" "$@"
